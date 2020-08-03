a0f4d0c35cf5a718ed0104e3171b8e2125f2353c
Luiz Gustavo Klitzke <49824305+luizklitzke1@users.noreply.github.com>
2020-08-03 16:27:49 -0300

    Form de registro em HTML basico e JS

--- front_end/js/meuscript.js
+++ front_end/js/meuscript.js
@@ -98,4 +98,19 @@ $(function() { // quando o documento estiver pronto/carregado
     if($("#corpoTabelaPersonagens").length){
         exibir_personagens()
     }; 
+
+    
 });
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('#test')
+          .attr('src', e.target.result)
+          .width(200)
+          .height(200);
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  };
